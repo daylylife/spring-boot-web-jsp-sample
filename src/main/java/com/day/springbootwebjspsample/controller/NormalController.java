6e0e7a7dd7a06951db57a1ee1fe511b05e81dc92
daianyu <daianyu@163.com>
2021-06-26 23:03:59 +0800

    hot-fix1 测试IDEA SPING BOOT项目，提交到仓库

--- src/main/java/com/day/springbootwebjspsample/controller/NormalController.java
+++ src/main/java/com/day/springbootwebjspsample/controller/NormalController.java
@@ -1,8 +1,13 @@
 package com.day.springbootwebjspsample.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class NormalController {
 
+	@RequestMapping("/hot-fix")
+	public String index(){
+		return "hot-fiex";
+	}
 }
